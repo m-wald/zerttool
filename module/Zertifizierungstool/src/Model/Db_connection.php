f1e128c8a9051f836e74050321f07d0b34be5d4f
m-wald <martin_waldmann@gmx.de>
2016-11-30 22:28:44 +0100

    Datenbank -> Model -> Controller -> View -Test

--- module/Zertifizierungstool/src/Model/Db_connection.php
+++ module/Zertifizierungstool/src/Model/Db_connection.php
@@ -8,28 +8,20 @@ class Db_connection
 	private $user 		= "root";
 	private $password 	= "zert4tool";
 	
-	private $conn		= NULL;
 	private $result;
 	
-	public function connect()
-	{
-		if ($this->conn == NULL) {
-			$this->conn = new \mysqli($this->server, $this->user, $this->password, $this->database);
-		}
+
+	
+	
+	public function execute($query) {
+		$conn = mysqli_connect($this->server, $this->user, $this->password, $this->database);
 		
 		if ($this->conn->connect_error)
 		{
-			die("Es konnte keine Verbindung zur Datenbank hergestellt werden: " . $this->conn->connect_error);	
+			die("Es konnte keine Verbindung zur Datenbank hergestellt werden: " . $this->conn->connect_error);
 		}
 		
-		return $this->conn;
-	}
-	
-	public function execute($query) {
-		if ($this->conn == NULL) {
-			$this->connect();
-		}
-		$this->result = $this->conn->query($query);
+		$this->result = mysqli_query($conn, $query);
 	}
 	
 	public function nextRow() {
