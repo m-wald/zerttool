9d5827c010f8b66ca618b9b3df99debf9510f184
Michael <Michael@LAPTOP-O26BCPC1.Speedport_W_724V_01011603_00_008>
2016-12-04 11:39:11 +0100

    test user model

--- module/Zertifizierungstool/src/Controller/UserController.php
+++ module/Zertifizierungstool/src/Controller/UserController.php
@@ -31,7 +31,7 @@ class UserController extends AbstractActionController
 	
 	public function registertestAction()
 	{
-		$user = new User("michi", "123", "Michael", "Moertl", "26.11.", "Nibelungenstrasse", "passau", "mich@mo.bla", 0, 1, 0, 0);
+		$user = new User("michi", "123", "Michael", "Moertl", "1990-11-26", "Nibelungenstrasse", "passau", "mich@mo.bla", 0, 1, 0, 0);
 	
 		$user->register();
 		$test = $user->alreadyExist();
