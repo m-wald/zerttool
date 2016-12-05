77a0b734ce946da7b55890d5e86cd4b82f6ce498
Michael <Michael@132.231.166.247>
2016-12-05 11:21:10 +0100

    sdf

--- module/Zertifizierungstool/src/Model/User.php
+++ module/Zertifizierungstool/src/Model/User.php
@@ -104,9 +104,9 @@ class User
 		
 		if (!$this->alreadyExist()){
 		$query = "insert into benutzer (benutzername, passwort, vorname, nachname, geburtsdatum, strasse, plz, ort, email, email_bestaetigt, ist_admin, ist_zertifizierer, ist_teilnehmer) values ('"
-				.$this->benutzername."', '".$this->passwort."', '".$this->vorname."', '".$this->nachname."', '"
-				.$this->geburtsdatum."', '".$this->strasse."', '".$this->plz."', '".$this->ort."', '".$this->email."', ".$this->email_bestaetigt.", "
-				.$this->ist_admin.", ".$this->ist_zertifizierer.", ".$this->ist_teilnehmer.");";
+  				.$this->benutzername."', '".$this->passwort."', '".$this->vorname."', '".$this->nachname."', '"
+  				.$this->geburtsdatum."', '".$this->strasse."', '".$this->plz."', '".$this->ort."', '".$this->email."', ".$this->email_bestaetigt.", "
+  				.$this->ist_admin.", ".$this->ist_zertifizierer.", ".$this->ist_teilnehmer.");";
 		
 		
 		$result = $db->execute($query);
