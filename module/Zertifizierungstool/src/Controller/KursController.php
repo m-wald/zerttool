ed8421e3d422c14152f477878271767570387cfc
SJ <SJ@192.168.2.105>
2016-12-10 14:26:08 +0100

    Kurscontroller angepasst

--- module/Zertifizierungstool/src/Controller/KursController.php
+++ module/Zertifizierungstool/src/Controller/KursController.php
@@ -11,6 +11,17 @@ class KursController extends AbstractActionController
     public function anlegenAction(){
         
         if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            
+            //Prüfung, ob Kursstartdatum vor -enddatum
+            if($_REQUEST("kursstart") > $_REQUEST("kursende")){
+                $error = "Das Kursendedatum liegt vor dem Kursstartdatum!";
+                return new ViewModel(['error' => $error]);
+            }
+            
+            
+            //todo Enddatum in der Zukunft abprüfen?
+            
+            
             $kurs = new Kurs(
                     $_REQUEST["kursname"], 
                     $_REQUEST["kursstart"], 
