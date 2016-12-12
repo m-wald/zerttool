4faa14d8fd8a40a632a493a1c04d8bfc9002e381
SJ <SJ@192.168.44.10>
2016-12-12 11:16:29 +0100

    anlegentest anlegen

--- module/Zertifizierungstool/src/Model/Kurs.php
+++ module/Zertifizierungstool/src/Model/Kurs.php
@@ -73,12 +73,7 @@ class Kurs {
      */
     public function save(){
         $db = new Db_connection();
-	$query = "INSERT INTO kurs (kurs_name, kurs_start, kurs_ende, sichtbarkeit, benutzername) VALUES ('".
-                $this->kurs_name."', '".
-                $this->kurs_start."', '".
-                $this->kurs_ende."', '".
-                $this->sichtbarkeit."', '".
-                $this->benutzername."')";
+	$query = "INSERT INTO kurs (kurs_name, kurs_start, kurs_ende, sichtbarkeit, benutzername) VALUES ('".$this->kurs_name."','".$this->kurs_start."', '".$this->kurs_ende."', '".$this->sichtbarkeit."', '".$this->benutzername."')";
         
 	$result = $db->execute($query);
                
