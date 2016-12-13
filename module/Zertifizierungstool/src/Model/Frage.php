bd36ac901ccacd467fa4adf31d97cf30f4d04569
m-wald <martin_waldmann@gmx.de>
2016-12-13 11:53:53 +0100

    Antwort speichern TF

--- module/Zertifizierungstool/src/Model/Frage.php
+++ module/Zertifizierungstool/src/Model/Frage.php
@@ -20,6 +20,7 @@ class Frage {
 	}
 	public function saveNew() {
 		$db = new Db_connection();
+		$conn = $db->getConnection();
 		
 		$query = "INSERT INTO frage (frage_text, punkte, pruefung_id, frage_typ) VALUES ('"
 				.$this->text 	. "', "
@@ -27,7 +28,7 @@ class Frage {
 				.$this->pruefung_id . ", '"
 				.$this->typ . "')" ;
 		
-		$result = $db->execute($query);
+		$result = mysqli_query($conn, $query);
 		
 		if(!$result) {
 			// Fehler bei der Datenbankabfrage
