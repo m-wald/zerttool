0f926099a83f366ebfb4eeaed389589d9c872f5e
maks-ua <maksfilonenko23@gmail.com>
2016-12-20 20:48:44 +0100

    ljk

--- module/Zertifizierungstool/src/Controller/KursController.php
+++ module/Zertifizierungstool/src/Controller/KursController.php
@@ -256,7 +256,7 @@ public function uploadAction(){
 		$new_path = $path.$kurs_id.'/'.$filename.'.'.$extension;
 		echo "Gespeichert in: ".$new_path."<br>";
 		if($path_new!=$new_path) echo "Wieder falsches UploadVerzeichnis!"."<br>";
-		if($_REQUEST["kurs_id"]!=NULL) echo $_REQUEST["kurs_id"];
+		if($_REQUEST["kurs_id"]==NULL) echo $_REQUEST["kurs_id"];
 		else "Kurs_id ist gleich NULL!";
 		 
 		//Neuer Dateiname falls die Datei bereits existiert
