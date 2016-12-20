c71a40324c6cf7f7a48f3c0a45d2810ddc734f29
maks-ua <maksfilonenko23@gmail.com>
2016-12-20 18:12:11 +0100

    upload

--- module/Zertifizierungstool/src/Controller/KursController.php
+++ module/Zertifizierungstool/src/Controller/KursController.php
@@ -213,7 +213,7 @@ public function uploadAction(){
 		//Check ob Verzeichnis mit dem Kurs Id exists
 		//Wenn nein - erstellt neues
 		$path= 'data/uploadsKurse/';
-		$path_new = $path.$id;
+		$path_new = $path.$id.'/';
 		
 		if(var_dump(is_dir($path_new)))
 			$upload_folder = $path_new;
