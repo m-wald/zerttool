f9140feb2ba81aa3f7eb17006630d6a9b4719cba
maks-ua <maksfilonenko23@gmail.com>
2016-12-20 13:32:45 +0100

    fd

--- module/Zertifizierungstool/src/Controller/KursController.php
+++ module/Zertifizierungstool/src/Controller/KursController.php
@@ -120,7 +120,7 @@ class KursController extends AbstractActionController
     public function kursviewAction(){
     	$id = $_GET["kurs_id"];
     	$kurs = new Kurs();
-    	if(!$kurs->load($id)) $status="Fehler beim Laden des Kurses!";
+    	if($kurs->load($id)) $status="Fehler beim Laden des Kurses!";
     	else $kursview = $kurs->load($id);
     	return new ViewModel(['kursview'=>$kursview]);
     }
