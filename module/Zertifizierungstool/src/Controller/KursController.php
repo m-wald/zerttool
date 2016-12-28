8181a60862ed16a949a904c1e46e15b8d59122f7
maks-ua <maksfilonenko23@gmail.com>
2016-12-28 15:42:44 +0100

    sadfc

--- module/Zertifizierungstool/src/Controller/KursController.php
+++ module/Zertifizierungstool/src/Controller/KursController.php
@@ -389,12 +389,10 @@ class KursController extends AbstractActionController
    	
     	$kurs= new Kurs();
     	if ($kurs->active($_REQUEST['kurs_id']) == 2){
-    		$zahl = 2;
-    		return new ViewModel(['meldung' =>'dateerror', 'zahl'=>$zahl]);
+    		return new ViewModel(['meldung' =>'dateerror']);
     	}
     	elseif($kurs->active($_REQUEST['kurs_id']) == 0) {
-    		$zahl = 1;
-    		return new ViewModel(['meldung' =>'datefuture', 'zahl'=>$zahl]);
+    		return new ViewModel(['meldung' =>'datefuture']);
     	}
    	
     	
