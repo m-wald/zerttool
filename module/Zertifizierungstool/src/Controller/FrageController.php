bc3b87a51742c883fef94c29e82b7748fd09c7a0
m-wald <martin_waldmann@gmx.de>
2016-12-29 14:09:42 +0100

    sadf

--- module/Zertifizierungstool/src/Controller/FrageController.php
+++ module/Zertifizierungstool/src/Controller/FrageController.php
@@ -18,26 +18,27 @@ use Zertifizierungstool\Model\Antwort;
  */
 class FrageController extends AbstractActionController {
 	
-	private $frage;
+	private $frage = new Frage();
 	
 	public function answerAction() {
 		// Frage laden
-		$frage = new Frage();
-		// TODO$frage->load()
+		$frageid = $this->params()->fromRoute('id');
+		$this->frage->load($frageid);
+		
 		// Alle Antworten zu dieser Frage laden
-		$antworten = Antwort::loadList($frage->getId());
+		$antworten = Antwort::loadList($this->frage->getId());
 		// Alle Fragen zu frage->getPruefungId() laden
-		$fragen = Frage::loadList($frage->getPruefungId());
+		$fragen = Frage::loadList($this->frage->getPruefungId());
 		// Array nach Id sortieren
 		array_multisort($fragen);
 		// Ermitteln der nächsten Id nach der aktuellen im Array
 			// Was bei letzter Id?
 		// Nachdem Formular angesendet wurde:
 			// Entsprechenden Eintrag in beantwortet ändern
-			// Ermitteln der nächsten Prüfung im Array
+			// Ermitteln der nächsten Frage im Array
 			
 		return new ViewModel([
-				'frage'		=> $frage,
+				'frage'		=> $this->frage,
 				'antworten' => $antworten,
 				'nextId'	=> $nextId,
 		]);
@@ -50,7 +51,6 @@ class FrageController extends AbstractActionController {
 		$errors = array();
 		
 		// Diesen Teil doch in die Methoden?
-		$this->frage = new Frage();
 		$pruefung = new Pruefung();
 		if ($mode == PruefungController::editFragen) {
 			$frageid = $_REQUEST["id"];
@@ -150,218 +150,12 @@ class FrageController extends AbstractActionController {
 	public function editAction() {
 		// Berechtigungsprüfung
 		if (!User::currentUser()->istAdmin() && !User::currentUser()->istZertifizierer()) {
-			array_push($errors, "Keine Berechtigung!");
+			header ("refresh:0; url = /user/login/");
 		}
 	
 		return $this->handleForm($_REQUEST, PruefungController::editFragen);
 	}
-	
-	
-	/*
-	public function createAction() {
-		// Prüfen ob Prüfungstermin schon erreicht ist
-		// Die Prüfung kann dann nicht mehr bearbeitet werden
-		
-		// Array, das eventuelle Fehlermeldungen enthält
-		$errors = array();
-		
-		// Berechtigungsprüfung
-		if (!User::currentUser()->istAdmin() && !User::currentUser()->istZertifizierer()) {
-			array_push($errors, "Keine Berechtigung!");
-		}
-		
-		// Erzeugung des Frage-Objekts mit Übergabe der zugehörigen Prüfungs-Id
-		$pruefungid = $_REQUEST["pruefung_id"];
-		
-		if (empty($pruefungid)) {
-			$pruefungid = $this->params()->fromRoute('id');
-		}
-		
-		$pruefung = new Pruefung();
-		
-		if (!$pruefung->load($pruefungid)) {
-			array_push($errors, "Fehler beim Laden der Prüfung!");
-		}
-		
-		if (is_bool($fragen)) {
-			array_push($errors, "Fehler beim Laden der Prüfungsfragen!");
-		}
 
-		
-		if ($_REQUEST['speichernFrage']) {
-			
-			$frage = new Frage("",
-					$_REQUEST["frage_text"],
-					$_REQUEST["punkte"],
-					$_REQUEST["pruefung_id"],
-					$_REQUEST["frage_typ"]);
-			
-			if (empty($errors)) {
-				if (!$frage->saveNew()) {
-					array_push($errors, "Fehler beim Speichern der Frage. Bitte erneut versuchen!");
-				}
-			}
-			if (empty($errors)) {
-				switch ($_REQUEST["frage_typ"]) {
-					case "TF":
-						$status = 0;
-						if ($_REQUEST["tf"] == "true") {
-							$status = 1;
-						}
-					
-						$antwort = new Antwort("", "", $frage->getId(), $status);
-						if (!$antwort->saveNew()) {
-							array_push($errors, "Fehler beim Speichern der Antwort. Bitte erneut versuchen!");
-						}
-					
-						break;
-				
-					case "MC":
-						$index = 1;
-						while (!empty($_REQUEST["antwort_text" .$index])) {
-							$status = 0;
-							if ($_REQUEST["antwort_checked" .$index]) {
-								$status = 1;
-							}
-						
-							$antwort = new Antwort("",
-									$_REQUEST["antwort_text" .$index],
-									$frage->getId(),
-									$status);
-						
-							if (!$antwort->saveNew()) {
-								array_push($errors, "Fehler beim Speichern der Antworten. Bitte erneut versuchen!");
-							}
-						
-							$index++;
-						}
-						break;
-					
-					default: break;
-				}
-			}	
-		}
-	
-		$viewModel = new ViewModel([
-				'pruefung' => $pruefung,
-				'fragen'   => Frage::loadList($pruefung->getId()),
-				'errors'   => $errors,
-				'mode'	   => PruefungController::createFragen
-		]);
-		
-		$viewModel->setTemplate(PruefungController::pathToHtml);
-		return $viewModel;
-	}
-	public function editAction() {
-		// Prüfen ob Prüfungstermin schon erreicht ist
-		// Die Prüfung kann dann nicht mehr bearbeitet werden
-		
-		// Array, das eventuelle Fehlermeldungen enthält
-		$errors = array();
-		
-		// Berechtigungsprüfung
-		if (!User::currentUser()->istAdmin() && !User::currentUser()->istZertifizierer()) {
-			array_push($errors, "Keine Berechtigung!");
-		}
-		
-		// Erzeugung des Frage-Objekts mit Übergabe der zugehörigen Prüfungs-Id
-		$frageid = $_REQUEST["id"];
-		
-		if (empty($frageid)) {
-			$frageid = $this->params()->fromRoute('id');
-		}
-		
-		$frage = new Frage();
-		$frage->load($frageid);
-		
-		$pruefung = new Pruefung();		
-		if (!$pruefung->load($frage->getPruefungId())) {
-			array_push($errors, "Fehler beim Laden der Prüfung!");
-		}		
-		
-		if ($_REQUEST['speichernFrage']) {
-				
-			$frage = new Frage(
-					$_REQUEST["id"],
-					$_REQUEST["frage_text"],
-					$_REQUEST["punkte"],
-					$_REQUEST["pruefung_id"],
-					$_REQUEST["frage_typ"]);
-				
-			if (empty($errors)) {
-				if (!$frage->update()) {
-					array_push($errors, "Fehler beim Speichern der Frage. Bitte erneut versuchen!");
-				}
-			}
-			if (empty($errors)) {
-				switch ($_REQUEST["frage_typ"]) {
-					case "TF":
-						$status = 0;
-						if ($_REQUEST["tf"] == "true") {
-							$status = 1;
-						}
-							
-						$antwort = new Antwort($_REQUEST["antwort_id"], "", $frage->getId(), $status);
-						if (empty($_REQUEST["id"])) {
-							if (!$antwort->saveNew()) {
-								array_push($errors, "Fehler beim Speichern der Antwort. Bitte erneut versuchen!");
-							}
-						} else {
-							if (!$antwort->update()) {
-								array_push($errors, "Fehler beim Speichern der Antwort. Bitte erneut versuchen!");
-							}
-						}
-						
-							
-						break;
-		
-					case "MC":
-						$index = 1;
-						while (!empty($_REQUEST["antwort_text" .$index])) {
-							$status = 0;
-							if ($_REQUEST["antwort_checked" .$index]) {
-								$status = 1;
-							}
-		
-							$antwort = new Antwort($_REQUEST["antwort_id" .$index],
-									$_REQUEST["antwort_text" .$index],
-									$frage->getId(),
-									$status);
-		
-							if (empty($_REQUEST["id"])) {
-								if (!$antwort->saveNew()) {
-									array_push($errors, "Fehler beim Speichern der Antworten. Bitte erneut versuchen!");
-								}
-							} else {
-								if (!$antwort->update()) {
-									array_push($errors, "Fehler beim Speichern der Antworten. Bitte erneut versuchen!");
-								}
-							}
-		
-							$index++;
-						}
-						break;
-				}
-			}
-			
-			if (empty($errors)) {
-				header ("refresh:0; url = /frage/create/" .$frage->getPruefungId());
-			}
-		}
-		
-		$viewModel = new ViewModel([
-				'pruefung' => $pruefung,
-				'fragen'   => Frage::loadList($pruefung->getId()),
-				'errors'   => $errors,
-				// Fragen laden -> Was bei Fehler?
-				'mode'	   => PruefungController::editFragen,
-				'frageToEdit' => $frage
-		]);
-		
-		$viewModel->setTemplate(PruefungController::pathToHtml);
-		return $viewModel;
-	}
-	*/
 	
 	public function deleteAction() {
 		// TODO Berechtigungsprüfungen
--- module/Zertifizierungstool/src/Controller/PruefungController.php
+++ module/Zertifizierungstool/src/Controller/PruefungController.php
@@ -9,6 +9,7 @@ use Zertifizierungstool\Model\Pruefung;
 use Zertifizierungstool\Model\Kurs;
 use Zertifizierungstool\Model\User;
 use Zertifizierungstool\Model\Frage;
+use Zertifizierungstool\Model\Antwort;
 
 /**
  * Controller, der Aufgaben verarbeitet, die sich auf die Entität "Prüfung" beziehen.
@@ -25,21 +26,33 @@ class PruefungController extends AbstractActionController {
 	const editFragen	 = "Fragen bearbeiten";
 	
 	/** Das behandelte Prüfungs-Objekt */
-	private $pruefung;
+	private $pruefung = new Pruefung();
 	
 	public function takeExamAction() {
-		// Prüfung mit Id aus URL laden
+		// Prüfungs-Id aus URL laden
+		$pruefung_id = $this->params()->fromRoute('id');
+		
 		// Eintrag in Tabelle schreibt_pruefung
-			//pruefung-getId(), currentUser()-getBenutzername(), aktueller timestamp, 0(nicht bestanden)
+			//$pruefung_id, currentUser()-getBenutzername(), aktueller timestamp, 0(nicht bestanden)
 		
 		// Alle Fragen zur Prüfung laden
+		$fragen = Frage::loadList($pruefung_id);
+		
 		// Für jede Frage:
+		foreach ($fragen as $frage) {
 			// Alle Antworten laden
+			$antworten = Antwort::loadList($frage->getId());
+			
 			// Für jede Antwort:
+			foreach ($antworten as $antwort) {
 				// Objekt von "beantwortet" erzeugen mit schreibt_pruefung->getId(), antwort->getId(), beantwortet_status = 0 ->in Db speichern
 				// extra-Attribut "edited" (gesetzt sobal User auf "Weiter" oder so geklickt hat)
-		
+			
+			}
+		}
+
 		// Weiterleiten an FrageController Action answer mit Id der ersten Prüfungsfrage
+		header("refresh:0; url = /frage/answer/" .$fragen[0]->getId());
 	}
 	
 	/**
@@ -150,131 +163,7 @@ class PruefungController extends AbstractActionController {
 		return $this->handleForm($_REQUEST, Frage::loadList($this->pruefung->getId()));
 	}
 	
-	/*
-	public function createAction() {
-		// Array, das eventuelle Fehlermeldungen enthält
-		$errors = array();
-		$result = false;
-		$fragen = array();
-		
-		// Berechtigungsprüfung weiterleitung auf fehlerseite
-		if (!User::currentUser()->istAdmin() && !User::currentUser()->istZertifizierer()) {
-			array_push($errors, "Keine Berechtigung!");
-		}
-
-		$newKursid = $_REQUEST["kursid"];
-		
-		if (empty($newKursid)) {
-			$newKursid = $this->params()->fromRoute('id');
-		}
-		
-		$pruefung = new Pruefung();
-		$pruefung->setKursId($newKursid);
-				
-		
-		if ($_REQUEST['speichernPruefung']) {
-			
-			$pruefung = new Pruefung("", 
-					$_REQUEST["name"],
-					$_REQUEST["termin"], 
-					$_REQUEST["kursid"], 
-					$_REQUEST["cutscore"] / 100 );
-			
-			// Format des Prüfungstermins überprüfen
-			// Prüfungstermin validieren
-			//array_push($errors, $this->checkDate($pruefung));
-			
-			if (empty($errors)) {
-				if (!$pruefung->saveNew()) {
-					array_push($errors, "Fehler beim Speichern der Pr&uuml;fung. Bitte erneut versuchen!");
-				}else {
-					header ("refresh:0; url = /frage/create/" .$pruefung->getId());
-					//$result = true;
-				}
-			}
-		}
-			
-		$viewModel = new ViewModel([
-				'pruefung' => $pruefung,
-				'errors'   => $errors,
-				'fragen'   => $fragen,
-				'mode'	   => PruefungController::createPruefung
-		]);
-		
-
-		$viewModel->setTemplate(PruefungController::pathToHtml);
-		return $viewModel;
-		
-	}
 	
-	
-	public function editAction() {
-		
-		// Prüfen ob Prüfungstermin schon erreicht ist
-		// Die Prüfung kann dann nicht mehr bearbeitet werden
-		
-		// Array, das eventuelle Fehlermeldungen enthält
-		$errors = array();
-		$result = false;
-		
-		// Berechtigungsprüfung
-		if (!User::currentUser()->istAdmin() && !User::currentUser()->istZertifizierer()) {
-			return "Keine Berechtigung";
-			//array_push($errors, "Keine Berechtigung!");
-		}
-
-		
-		if (!$_REQUEST['speichernPruefung']) {
-			// Formular wurde noch nicht gesendet
-			$pruefung_id = $this->params()->fromRoute('id');
-			$pruefung = new Pruefung();
-			$pruefung->load($pruefung_id);
-			$kurs = new Kurs();
-			$kurs->load($pruefung->getKursId());
-			
-			if (!$kurs->getBenutzername() == User::currentUser()->getBenutzername()) {
-				array_push($errors, "Keine Berechtigung!");
-			}
-			
-			if ($pruefung->getTermin() >= new Date()) {
-				array_push($errors, "Der Prüfungszeitraum wurde bereits erreicht. Die Prüfung kann nicht mehr bearbeitet werden!");
-			}
-			
-		} else {
-				
-			$pruefung = new Pruefung(
-					$_REQUEST["pruefid"],
-					$_REQUEST["name"],
-					$_REQUEST["termin"],
-					$_REQUEST["kursid"],
-					$_REQUEST["cutscore"] / 100 );
-				
-			// Format des Prüfungstermins überprüfen
-			// Prüfungstermin validieren
-			//array_push($errors, $this->checkDate($pruefung));
-				
-			if (empty($errors)) {
-				if (!$pruefung->update()) {
-					array_push($errors, "Fehler beim Speichern der Pr&uuml;fung. Bitte erneut versuchen!");
-				}else {
-					header ("refresh:0; url = /frage/create/" .$pruefung->getId());
-					//$result = true;
-				}
-			}
-		}
-			
-		$viewModel = new ViewModel([
-				'pruefung' => $pruefung,
-				'errors'   => $errors,
-				'fragen'   => Frage::loadList($pruefung->getId()),
-				'mode'	   => PruefungController::editPruefung
-		]);
-		
-		
-		$viewModel->setTemplate(PruefungController::pathToHtml);
-		return $viewModel;
-	}
-	*/
 	
 	//TODO
 	/**
