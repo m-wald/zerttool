078a1829ebd7a080d512296afabebab1f1113231
m-wald <martin_waldmann@gmx.de>
2017-01-03 16:28:50 +0100

    ecjo

--- module/Zertifizierungstool/src/Controller/PruefungController.php
+++ module/Zertifizierungstool/src/Controller/PruefungController.php
@@ -232,6 +232,7 @@ class PruefungController extends AbstractActionController {
 				$error= "Der Pr&uuml;fungszeitraum kann erst nach Kursbeginn starten!";
 			
 			}elseif ($this->pruefung->getTermin() > date_sub(strtotime($kurs->getKurs_ende()), new \DateInterval("P4D"))) {
+				echo date_sub(strtotime($kurs->getKurs_ende()), new \DateInterval("P4D"));
 				$error = "Der Pr&uuml;fungszeitraum muss mindestens 4 Tage vor Kursende starten!";
 			}
 		}else {
