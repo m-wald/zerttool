3cdec1e7a93d6f482c8791832671b1fc884068a8
SJ <SJ@192.168.44.10>
2017-01-04 16:44:18 +0100

    loadKurse anpassen

--- module/Zertifizierungstool/src/Model/Kurs.php
+++ module/Zertifizierungstool/src/Model/Kurs.php
@@ -80,12 +80,12 @@ class Kurs {
     
     	
         
-        if (mysqli_num_rows($result) > 0){
+        //if (mysqli_num_rows($result) > 0){
         	// TODO Ihr �bergebt hier direkt das Ergebnis der Datenbankabfrage.
         	// Besser w�rs, wenn ihr f�r jeden Datensatz ein neues Objekt von der Klasse "Kurs" anlegt und in ein Array speichert.
         	// Am Ende k�nnt ihr dann das Array zur�ckgeben und in der View dann einfach die getter-Methoden f�r das jeweilige Objekt aufrufen.
         	// #Objektorientierung ;)
-        /*if (mysqli_num_rows($result) > 0) {
+        if (mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
                     array_push($return_array, $row);
             }
@@ -101,8 +101,8 @@ class Kurs {
             }
             
             return $return_array;
-        */ 
-            return $result;
+         
+            //return $result;
         } else {
             //kein Ergebnis gefunden
             return 0;
