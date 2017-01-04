40878da3c3883e29cf1ce2c372c7e1633bed85d8
SJ <SJ@192.168.44.10>
2017-01-04 17:42:22 +0100

    Kurs anpassen Objektorientierung

--- module/Zertifizierungstool/src/Model/Kurs.php
+++ module/Zertifizierungstool/src/Model/Kurs.php
@@ -86,13 +86,13 @@ class Kurs {
         if (mysqli_num_rows($result) > 0) {
             $return_array = array();
             while ($row = mysqli_fetch_assoc($result)) {
-                    $kurse = new Kurs(
+                    $kurse = new Kurs();/*
                             $row["kurs_id"],
                             $row["kurs_name"],
                             $row["kurs_start"],
                             $row["kurs_ende"],
                             $row["sichtbarkeit"],
-                            $row["benutzername"]);
+                            $row["benutzername"]);*/
                     
                     array_push($return_array, $kurse);
             }
