4419c17c433ef26b00c29e1517df4612aceef637
maks-ua <maksfilonenko23@gmail.com>
2017-01-09 19:06:30 +0100

    asd

--- module/Zertifizierungstool/src/Controller/KursController.php
+++ module/Zertifizierungstool/src/Controller/KursController.php
@@ -628,7 +628,7 @@ class KursController extends AbstractActionController
     		exit;
     	}
     	
-    	if(!$_POST['site']=="kursview" || !$_SESSION['site'] == 'delete') {
+    	if(!$_POST['site']=="kursview" && !$_SESSION['site'] == 'delete') {
     		header("refresh:0; url= /user/home");
     		exit;
     	}
