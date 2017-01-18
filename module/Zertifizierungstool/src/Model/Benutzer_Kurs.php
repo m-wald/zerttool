116fcd9a1226816eec52b01b801eb9b6039c7306
Michael <Michael@Michael-PC>
2017-01-18 19:00:06 +0100

    test

--- module/Zertifizierungstool/src/Model/Benutzer_Kurs.php
+++ module/Zertifizierungstool/src/Model/Benutzer_Kurs.php
@@ -44,9 +44,8 @@ class Benutzer_Kurs {
 		$query_anzahl_bestandene_pruefungen = "select count(*) from schreibt_pruefung join pruefung using (pruefung_id) where kurs_id =".$kurs_id." and benutzername = '".$benutzer."' and bestanden=1";
 		$anzahl_pruefungen = $db->execute($query_anzahl_pruefungen);
 		$anzahl_bestandene_pruefungen = $db->execute($query_anzahl_bestandene_pruefungen);
-		echo $anzahl_pruefungen;
-		echo $anzahl_bestandene_pruefungen;
-		if ($anzahl_bestandene_pruefungen<$anzahl_pruefungen) {
+		echo $anzahl_bestandene_pruefungen[0]." ".$anzahl_pruefungen[0];
+		if ($anzahl_bestandene_pruefungen[0]<$anzahl_pruefungen[0]) {
 		//Insert der Daten
 		
 			$query1="insert into benutzer_kurs(benutzername,kurs_id) values('".$benutzer."',".$kurs_id.");";
