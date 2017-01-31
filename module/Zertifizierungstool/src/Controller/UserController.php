44f42e92377f56562cbd7ba0d5a4ae2a0bc90b0a
Michael <Michael@Michael-PC>
2017-01-31 20:44:54 +0100

    test validatedate() rückgängig

--- module/Zertifizierungstool/src/Controller/UserController.php
+++ module/Zertifizierungstool/src/Controller/UserController.php
@@ -63,7 +63,7 @@ class UserController extends AbstractActionController
 			//Überprüfung ob Passwort und bestätigtes Passwort übereinstimmen
 			if ($_REQUEST['passwort']==$_REQUEST['passwort2']) {
 				$currentdate = date('Y-m-d');
-				if((Kurs::validateDate($_REQUEST["geburtsdatum"]) || Kurs::validateDate($_REQUEST["geburtsdatum"], 'd.m.Y')) && (strtotime($_REQUEST["geburtsdatum"])<=strtotime($currentdate))) {
+				if(strtotime($_REQUEST["geburtsdatum"])!=false && (strtotime($_REQUEST["geburtsdatum"])<=strtotime($currentdate))) {
 					
 					
 					//Bei Registrierung über invite, wird ein angepasster Registrierungslink
